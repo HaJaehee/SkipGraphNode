8a157daecd0163d1af4271b3e7af75b725a3a246
J.Ha <jaehee.ha@kaist.ac.kr>
2021-07-23 13:58:51 +0900

    Update start-node.sh

--- start-node.sh
+++ start-node.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # "Usage: java -jar target/SkipGraphNode-1.0-SNAPSHOT.jar [switch num] [locality id] [DHT node num] [introducer ip] [introducer port] logging"
 cd /home/wins/SkipGraphNode
+num=1
 cat /home/wins/SkipGraphNode/node_num | while read line
 do
         num=`expr $line + 1`
         echo $num > node_num
-        java -jar target/SkipGraphNode-1.0-SNAPSHOT.jar $line `hostname -f` 0 10.22.0.1 21100 logging
 done
+java -jar target/SkipGraphNode-1.0-SNAPSHOT.jar $num `hostname -f` 0 10.22.0.1 21100
